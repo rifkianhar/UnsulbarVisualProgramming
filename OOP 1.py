0c1238a2e9912d4ed24f90497e2424684c58c78f
rifkianhar <76133521+rifkianhar@users.noreply.github.com>
2022-03-17 16:29:33 +0700

    Add files via upload

---
+++ OOP 1.py
@@ -0,0 +1,15 @@
+class Buah:
+    pass
+
+buah1 = Buah()
+buah2 = Buah()
+
+buah1.nama = "apel"
+buah1.warna = "merah"
+buah1.banyak = "2 buah"
+
+buah2.nama = "mangga"
+buah2.warna = "kuning"
+buah2.banyak = "5 buah"
+
+print(buah1.__dict__)
